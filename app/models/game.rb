cb3e7130c5f2db052d6d17b5dab69c78da7ef29c
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-07 13:29:43 -0800

    Require a winner for a game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   validates :played_at, presence: true
+  validates :winner, presence: true
+
+  belongs_to :winner, class_name: User
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 
   before do
-    @valid_attributes = { played_at: Time.now }
+    @valid_attributes = { played_at: Time.now,
+                          winner: User.create(email: 'foo@regalii.com', password: 'secret123') }
   end
 
   it 'is sane' do
@@ -15,6 +16,9 @@ RSpec.describe Game, type: :model do
       expect(Game.new(@valid_attributes.merge(played_at: nil))).to_not be_valid
     end
 
+    it 'requires a winner' do
+      expect(Game.new(@valid_attributes.merge(winner: nil))).to_not be_valid
+    end
   end
   
 end
