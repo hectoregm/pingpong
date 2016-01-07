10da5a7ad4e7ae05b309f8e6bff9f26692f3a493
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-07 13:40:23 -0800

    Add loser and score fields and tests.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,10 @@
 class Game < ActiveRecord::Base
   validates :played_at, presence: true
   validates :winner, presence: true
+  validates :loser, presence: true
+  validates :winner_score, presence: true
+  validates :loser_score, presence: true
 
   belongs_to :winner, class_name: User
+  belongs_to :loser, class_name: User
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Game, type: :model do
 
   before do
     @valid_attributes = { played_at: Time.now,
-                          winner: User.create(email: 'foo@regalii.com', password: 'secret123') }
+                          winner: User.create!(email: 'foo@regalii.com', password: 'secret123'),
+                          loser: User.create!(email: 'bar@regalii.com', password: 'secret456'),
+                          winner_score: 21,
+                          loser_score: 19
+                        }
   end
 
   it 'is sane' do
@@ -19,6 +23,14 @@ RSpec.describe Game, type: :model do
     it 'requires a winner' do
       expect(Game.new(@valid_attributes.merge(winner: nil))).to_not be_valid
     end
+
+    it 'requires a loser' do
+      expect(Game.new(@valid_attributes.merge(loser: nil))).to_not be_valid
+    end
+
+    it 'require a score of each player' do
+      expect(Game.new(@valid_attributes.merge(winner_score: nil))).to_not be_valid
+      expect(Game.new(@valid_attributes.merge(loser_score: nil))).to_not be_valid
+    end
   end
-  
 end
